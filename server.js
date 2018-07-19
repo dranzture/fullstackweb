aa8524e7faf1284900ba2afe46a2f26b75946d59
Polat Coban <polatcoban@gmail.com>
2018-07-19 15:07:52 -0500

    https update

--- server.js
+++ server.js
@@ -1,9 +1,27 @@
+const https = require('https');
+
 const http = require('http');
+
 const app = require('./app');
+const fs = require('fs');
+
+const key = fs.readFileSync('./encryption/localhost.key');
+const cert = fs.readFileSync('./encryption/localhost.crt');
+const ca = fs.readFileSync('./encryption/ca.crt');
+
+const options = {
+    key: key,
+    cert: cert,
+    ca: ca
+};
 
 const port = process.env.PORT || 3000;
 
-const server = http.createServer(app);
+const server = https.createServer(options, app);
 server.listen(port, () => {
     console.log("Listening server at port: " + port);
 });
+// const server = http.createServer(app);
+// server.listen(port, () => {
+//     console.log("Listening server at port: " + port);
+// });
